17da55567bc35c939bb29565e3e53275150e9cec
henrynguyen1203@gmail.com <henrynguyen1203@gmail.com>
2024-06-06 16:22:06 +0700

    fix email regex and add clinic account login

--- src/main/java/com/example/dentalclinicschedulingplatform/utils/AppConstants.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/utils/AppConstants.java
@@ -7,5 +7,5 @@ public class AppConstants {
     public static final String DEFAULT_SORT_DIRECTION = "asc";
 
     public static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,12}$";
-    public static final String EMAIL_REGEX = "^[^\\.][a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,7}$\n";
+    public static final String EMAIL_REGEX = "^[^\\.][a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,7}$";
 }
