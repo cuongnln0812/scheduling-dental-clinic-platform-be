4dd441be0d67891cb24e5fa7b8a730d7d6f34d71
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-06-11 06:54:50 +0700

    [feat] Get feedback list all and by branchclinicID

--- src/main/java/com/example/dentalclinicschedulingplatform/controller/FeedbackController.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/controller/FeedbackController.java
@@ -2,7 +2,6 @@ package com.example.dentalclinicschedulingplatform.controller;
 
 import com.example.dentalclinicschedulingplatform.payload.request.SendFeedbackRequest;
 import com.example.dentalclinicschedulingplatform.payload.response.ApiResponse;
-import com.example.dentalclinicschedulingplatform.payload.response.CustomerRegisterResponse;
 import com.example.dentalclinicschedulingplatform.payload.response.SendFeedbackResponse;
 import com.example.dentalclinicschedulingplatform.service.IFeedbackService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,11 +9,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
