cbed8c5fe80d322fa53f8354789a5fcc3b0c879d
henrynguyen1203@gmail.com <henrynguyen1203@gmail.com>
2024-06-13 14:43:56 +0700

    fix update info

--- src/main/java/com/example/dentalclinicschedulingplatform/payload/request/UserInfoUpdateRequest.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/payload/request/UserInfoUpdateRequest.java
@@ -4,10 +4,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoUpdateRequest {
     @NotBlank(message = "Username must not be blank")
     private String username;
