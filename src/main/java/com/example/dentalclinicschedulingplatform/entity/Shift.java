e288055dccfa3ffd07a71fcb6197d8cc013ebc80
TuNguyen123vn <nguyentu100312@gmail.com>
2024-06-20 13:53:40 +0700

    [fix] Refresh token add admin

--- src/main/java/com/example/dentalclinicschedulingplatform/entity/Shift.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/entity/Shift.java
@@ -1,7 +1,9 @@
 package com.example.dentalclinicschedulingplatform.entity;
 
+import com.example.dentalclinicschedulingplatform.exception.ApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @ToString
@@ -12,5 +14,17 @@ public enum Shift {
     ;
 
     private final String shift;
+
+    public static void isValid(Shift shift) {
+        if (shift == null) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Status is not valid");
+        }
+        for (Shift s : Shift.values()) {
+            if (s.shift.equals(shift.name())) {
+                return;
+            }
+        }
+        throw new ApiException(HttpStatus.BAD_REQUEST, "Status is not valid");
+    }
 
 }
