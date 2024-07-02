702d9b4b0e26b07ca3f4541055740774c1f02db3
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-07-02 17:56:22 +0700

    [Update] Update send feedback api

--- src/main/java/com/example/dentalclinicschedulingplatform/service/impl/FeedbackService.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/service/impl/FeedbackService.java
@@ -15,6 +15,8 @@ import com.example.dentalclinicschedulingplatform.service.IFeedbackService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,22 +34,29 @@ public class FeedbackService implements IFeedbackService {
 
     @Override
     public SendFeedbackResponse sendFeedback(SendFeedbackRequest request) {
+        String username = getCurrentUsername();
+        Customer customer = customerRepository.findByUsername(username)
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "Customer not found"));
         Feedback feedback = new Feedback();
         feedback.setComment(request.getComment());
         feedback.setRating(request.getRating());
         feedback.setCreatedDateTime();
-        Customer customer = customerRepository.findById(request.getUserID())
-                .orElseThrow(() -> new ApiException ( HttpStatus.NOT_FOUND ,"Customer not found" ));
         feedback.setCustomer(customer);
-
         ClinicBranch clinicBranch = clinicBranchRepository.findById(request.getBranchclinicID())
-                .orElseThrow(() -> new ApiException ( HttpStatus.NOT_FOUND ,"Branch Clinic not found" ));
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "Branch Clinic not found"));
         feedback.setClinicBranch(clinicBranch);
         feedback = feedbackRepository.save(feedback);
-        SendFeedbackResponse response = modelMapper.map (feedback, SendFeedbackResponse.class);
+        SendFeedbackResponse response = modelMapper.map(feedback, SendFeedbackResponse.class);
         return response;
     }
-
+    private String getCurrentUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
+    }
     @Override
     public List<SendFeedbackResponse> getFeedbackByBranchId(Long branchId) {
         List<Feedback> feedbackList = feedbackRepository.findByClinicBranch_BranchId(branchId);
