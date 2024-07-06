9c9ffc112c3c047bd15a5d1f68674b3c77999dd5
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-07-06 23:22:27 +0700

    [Update] Update getAllReported and getReportbyClinic API

--- src/main/java/com/example/dentalclinicschedulingplatform/payload/response/ReportResponse.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/payload/response/ReportResponse.java
@@ -1,9 +1,17 @@
 package com.example.dentalclinicschedulingplatform.payload.response;
 
 import com.example.dentalclinicschedulingplatform.entity.ReportReason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReportResponse {
     private Long reportId;
     private Long feedbackId;
@@ -13,4 +21,13 @@ public class ReportResponse {
     private LocalDateTime createdDateTime;
     private Long clinicId;
     private Long branchId;
+    private String avatar;
+    private String fullName;
+    private String branchName;
+    private String city;
+    private String comment;
+    private Double rating;
+    private Double averageRating;
+    private Long totalFeedback;
+
 }
--- src/main/java/com/example/dentalclinicschedulingplatform/repository/FeedbackRepository.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/repository/FeedbackRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     List<Feedback> findByClinicBranch_BranchId(Long branchId);
+    List<Feedback> findByClinicBranch_Clinic_ClinicId(Long clinicId);
 }
