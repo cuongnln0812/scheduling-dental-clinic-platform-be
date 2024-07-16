6df9f1664575352c5a90bee81135baccf6e58205
henrynguyen1203@gmail.com <henrynguyen1203@gmail.com>
2024-07-16 10:56:56 +0700

    fix clinic update validation

--- src/main/java/com/example/dentalclinicschedulingplatform/service/impl/ClinicService.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/service/impl/ClinicService.java
@@ -104,6 +104,12 @@ public class ClinicService implements IClinicService {
                 .orElseThrow(() -> new ResourceNotFoundException("Clinic", "id", request.getClinicId()));
         if(clinic.getStatus().equals(ClinicStatus.INACTIVE) || clinic.getStatus().equals(ClinicStatus.PENDING))
             throw new ApiException(HttpStatus.CONFLICT, "You cannot update clinic information because this clinic is inactive or pending!");
+        if(clinicRepository.existsByEmail(request.getEmail()))
+            throw new ApiException(HttpStatus.BAD_REQUEST, "This email is already registered by another clinic!");
+        if(clinicRepository.existsByPhone(request.getPhone()))
+            throw new ApiException(HttpStatus.BAD_REQUEST, "This phone is already registered by another clinic!");
+        if(clinicRepository.existsByPhone(request.getWebsiteUrl()))
+            throw new ApiException(HttpStatus.BAD_REQUEST, "This website is already registered by another clinic!");
         clinic.setAddress(request.getAddress());
         clinic.setEmail(request.getEmail());
         clinic.setCity(request.getCity());
