0dacb93d7d65922a0bc1546e0a3d831f974e7a2d
TuNguyen123vn <nguyentu100312@gmail.com>
2024-07-16 21:14:11 +0700

    [fix] Modify update working hours

--- src/main/java/com/example/dentalclinicschedulingplatform/repository/SlotRepository.java
+++ src/main/java/com/example/dentalclinicschedulingplatform/repository/SlotRepository.java
@@ -17,6 +17,6 @@ public interface SlotRepository extends JpaRepository<Slot, Long> {
     List<Slot> findByClinicAndDay(String day, Long clinicId);
 
     @Query(value = "SELECT s.slot_id FROM slot s join working_hours wh on s.working_hours_id = wh.working_hours_id " +
-            " where s.clinic_id = :clinicId and s.status = true", nativeQuery = true)
-    List<Long> findSlotIdByClinic(Long clinicId);
+            " where s.clinic_id = :clinicId and wh.day = :day and s.status = true", nativeQuery = true)
+    List<Long> findSlotIdByClinic(String day,Long clinicId);
 }
